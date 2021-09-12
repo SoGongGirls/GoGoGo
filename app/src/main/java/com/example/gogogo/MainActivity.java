84e92f8c7de9cc63da79a0bc410a7defe5913754
YuseonChoi <chldbtjs74@gmail.com>
2021-09-12 16:37:13 +0900

    Create Drawer header

--- app/src/main/java/com/example/gogogo/MainActivity.java
+++ app/src/main/java/com/example/gogogo/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.gogogo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    NavigationView navigationView;
+    BottomNavigationView bottomNavigationView;
+    DrawerLayout drawerLayout;
+    View drawer;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_drawer, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
     }
 }
