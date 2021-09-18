7bf6095e305e2c77c40abf41065ebc24ca25cbb3
MinseoPark <kjms0820@gmail.com>
2021-09-18 11:11:33 +0900

    create store list fragment

--- app/src/main/java/com/example/gogogo/MainActivity.java
+++ app/src/main/java/com/example/gogogo/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.gogogo;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,6 +18,7 @@ import com.example.gogogo.fragment.Fragment2;
 import com.example.gogogo.fragment.Fragment3;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
     Fragment fragment1;
     Fragment fragment2;
     Fragment fragment3;
+
+    DeliveryList deliveryList;
+    StoreList storeList;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -82,44 +89,40 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
+        /* 매장 리스트 */
+        storeList = new StoreList();
+        deliveryList = new DeliveryList();
 
-        /*
-        // 매장 리스트 내 탭
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.layout_tab);
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, storeList).commit();
+
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("매장"));
+        tabs.addTab(tabs.newTab().setText("배달"));
+
+        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                // tab의 상태가 선택 상태로 변경
-                int pos = tab.getPosition();
-                changeView(pos);
+                int position = tab.getPosition();
+                Log.d("MainActivity", "선택된 탭 : " + position);
+
+                Fragment selected = null;
+                if (position == 0) {
+                    selected = storeList;
+                } else if (position == 1) {
+                    selected = deliveryList;
+                }
+
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.container, selected).commit();
             }
 
             @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-                // tab의 상태가 선택되지 않음으로 변경 - do nothing
-            }
+            public void onTabUnselected(TabLayout.Tab tab) { }
 
             @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-                // 이미 선택된 tab이 다시 선택 - do nothing
-            }
+            public void onTabReselected(TabLayout.Tab tab) { }
         });
-    }
 
-    private void changeView(int index) {
-        ListView listView1 = (ListView) findViewById(R.id.store_list);
-        ListView listView2 = (ListView) findViewById(R.id.delivery_list);
-
-        switch (index) {
-            case 0:
-                listView1.setVisibility(View.VISIBLE);
-                listView2.setVisibility(View.INVISIBLE);
-                break;
-            case 1:
-                listView1.setVisibility(View.INVISIBLE);
-                listView2.setVisibility(View.VISIBLE);
-                break;
-        }
-        */
     }
 }
