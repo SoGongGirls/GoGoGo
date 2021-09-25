93dac7d22d9cba0a0dd47e73129debc84e54d6ae
MinseoPark <kjms0820@gmail.com>
2021-09-25 14:38:11 +0900

    Make Diet Survey

--- app/src/main/java/com/example/gogogo/survey/Survey.java
+++ app/src/main/java/com/example/gogogo/survey/Survey.java
@@ -28,10 +28,16 @@ public class Survey extends AppCompatActivity {
     TextView select3Tv;
     TextView select4Tv;
 
+    int count1 = 0;   // 해산물 화면전환 조건
+    int count2 = 0;   // 면 화면전환 조건
+    int count3 = 0;   // 밥 화면전환 조건
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        question = (TextView) findViewById(R.id.question);
 
         select1Tv = (TextView) findViewById(R.id.select1_tv);
         select2Tv = (TextView) findViewById(R.id.select2_tv);
@@ -54,44 +60,113 @@ public class Survey extends AppCompatActivity {
         select4 = (ImageView) findViewById(R.id.select4);
         select4.setBackground(new ShapeDrawable(new OvalShape()));
         select4.setClipToOutline(true);
-
-
-        question = (TextView) findViewById(R.id.question);
     }
 
     public void selectOne(View view) {
-        question.setText("Q. 돼지 VS 소 VS 닭");
+        count1 += 1;
+        count3 += 1;
+        if (count1 == 2) {
+            question.setText("Q. 생선 VS 생선말고");
 
-        // 답변 사진
-        select1.setImageResource(R.drawable.pork);
-        select2.setImageResource(R.drawable.steak);
-        select3.setImageResource(R.drawable.chicken);
-        select4.setImageResource(R.drawable.lamb);
+            select1.setImageResource(R.drawable.eel);
+            select2.setImageResource(R.drawable.seafood);
+            select3.setImageResource(R.drawable.white);
+            select4.setImageResource(R.drawable.white);
 
-        // 답변 내용
-        select1Tv.setText("돼지고기");
-        select2Tv.setText("소고기");
-        select3Tv.setText("닭고기");
-        select4Tv.setText("다른거");
+            select1Tv.setText("생선");
+            select2Tv.setText("생선말고");
+            select3Tv.setText("");
+            select4Tv.setText("");
+        }
+        else if (count3 == 3) {
+            question.setText("Q. 뜨끈한국물과 VS 국물말고");
+
+            // 답변 사진
+            select1.setImageResource(R.drawable.jjigae);
+            select2.setImageResource(R.drawable.rice2);
+            select3.setImageResource(R.drawable.white);
+            select4.setImageResource(R.drawable.white);
+
+            // 답변 내용
+            select1Tv.setText("뜨끈한국물과");
+            select2Tv.setText("국물말고");
+            select3Tv.setText("");
+            select4Tv.setText("");
+        }
+        else {
+            question.setText("Q. 돼지 VS 소 VS 닭");
+
+            // 답변 사진
+            select1.setImageResource(R.drawable.pork);
+            select2.setImageResource(R.drawable.steak);
+            select3.setImageResource(R.drawable.chicken);
+            select4.setImageResource(R.drawable.lamb);
+
+            // 답변 내용
+            select1Tv.setText("돼지고기");
+            select2Tv.setText("소고기");
+            select3Tv.setText("닭고기");
+            select4Tv.setText("다른거");
+        }
     }
 
     public void selectTwo(View view) {
-        question.setText("Q. 익힌 VS 안익힌");
+        count1 += 1;
+        count2 += 1;
+        if (count2 == 2) {
+            question.setText("Q. 국물 VS 안국물");
 
-        // 답변 사진
-        select1.setImageResource(R.drawable.eel);
-        select2.setImageResource(R.drawable.fish);
-        select3.setImageResource(R.drawable.white);
-        select4.setImageResource(R.drawable.white);
+            // 답변 사진
+            select1.setImageResource(R.drawable.ramen);
+            select2.setImageResource(R.drawable.jajangmyeon);
+            select3.setImageResource(R.drawable.white);
+            select4.setImageResource(R.drawable.white);
 
-        // 답변 내용
-        select1Tv.setText("익힌");
-        select2Tv.setText("안익힌");
-        select3Tv.setText("");
-        select4Tv.setText("");
+            // 답변 내용
+            select1Tv.setText("국물");
+            select2Tv.setText("안국물");
+            select3Tv.setText("");
+            select4Tv.setText("");
+        }
+        else {
+            question.setText("Q. 익힌 VS 안익힌");
+
+            // 답변 사진
+            select1.setImageResource(R.drawable.eel);
+            select2.setImageResource(R.drawable.fish);
+            select3.setImageResource(R.drawable.white);
+            select4.setImageResource(R.drawable.white);
+
+            // 답변 내용
+            select1Tv.setText("익힌");
+            select2Tv.setText("안익힌");
+            select3Tv.setText("");
+            select4Tv.setText("");
+        }
+    }
+
+    public void selectThree(View view) {
+        count3 += 1;
+        if (count3 == 2) {
+            question.setText("Q. 밥 VS 죽");
+
+            // 답변 사진
+            select1.setImageResource(R.drawable.rice1);
+            select2.setImageResource(R.drawable.juk);
+            select3.setImageResource(R.drawable.white);
+            select4.setImageResource(R.drawable.white);
+
+            // 답변 내용
+            select1Tv.setText("밥");
+            select2Tv.setText("죽");
+            select3Tv.setText("");
+            select4Tv.setText("");
+        }
     }
 
     public void selectFour(View view) {
+        count2 += 1;
+        count3 += 1;
         question.setText("Q. 빵 VS 면 VS 밥 VS 떡·만두");
 
         // 답변 사진
