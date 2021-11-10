c234a1c0c1a3935d00843cb2a595640897b98e49
MinseoPark <kjms0820@gmail.com>
2021-11-10 14:33:56 +0900

    Change Location Button

--- app/src/main/java/com/example/gogogo/Roulette.java
+++ app/src/main/java/com/example/gogogo/Roulette.java
@@ -1,18 +1,27 @@
 package com.example.gogogo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
@@ -28,7 +37,10 @@ import com.example.gogogo.storeInfo.StoreRecommend;
 import com.example.gogogo.survey.SurveyMeal;
 import com.example.gogogo.survey.SurveyMealMeat;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class Roulette extends AppCompatActivity {
     private CircleManager circleManager;
@@ -42,6 +54,17 @@ public class Roulette extends AppCompatActivity {
     private float initAngle = 0.0f;
     private int num_roulette;
     public static String result3;
+
+
+    // 내 위치 확인
+    private GpsTracker gpsTracker;
+
+    private static final int GPS_ENABLE_REQUEST_CODE = 2001;
+    private static final int PERMISSONS_REQUEST_CODE = 100;
+    String[] REQUIRED_PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+
+    public static double myLatitude;   // 내 위치 위도
+    public static double myLongitude;   // 내 위치 경도
 
 
     @Override
@@ -68,6 +91,34 @@ public class Roulette extends AppCompatActivity {
                 rotateLayout(layoutRoulette, num_roulette);
             }
         });
+
+
+        // 내 위치 확인하기
+        if (!checkLocationServicesStatus()) {
+            showDialogForLocationServiceSetting();
+        } else {
+            checkRunTimePermission();
+        }
+
+        // final TextView textView2 = (TextView) findViewById(R.id.locationText);  // 내 위치 보여주는 텍스트뷰
+        Button locationButton = (Button) findViewById(R.id.locationButton);  // 내 위치 확인 버튼
+
+        gpsTracker = new GpsTracker(Roulette.this);
+
+        myLatitude = gpsTracker.getLatitude();
+        myLongitude = gpsTracker.getLongitude();
+
+        locationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String address = getCurrentAddress(myLatitude, myLongitude);
+//                textView2.setText(address);
+
+//                Toast.makeText(Roulette.this, "현재위치 \n위도 " + myLatitude + "\n경도 " + myLongitude,
+//                        Toast.LENGTH_LONG).show();
+            }
+        });
+
 
         // 내 주변 매장 확인하기
         result2.setOnClickListener(new View.OnClickListener() {
@@ -280,5 +331,119 @@ public class Roulette extends AppCompatActivity {
                 temp += sweepAngle;
             }
         }
+    }
+
+
+    // 내 위치 확인하기
+    @Override
+    public void onRequestPermissionsResult(int permsRequestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grandResults) {
+        super.onRequestPermissionsResult(permsRequestCode, permissions, grandResults);
+        if (permsRequestCode == PERMISSONS_REQUEST_CODE && grandResults.length == REQUIRED_PERMISSIONS.length) {
+            boolean check = true;
+
+            for (int result : grandResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    check = false;
+                    break;
+                }
+            }
+
+            if (check) {
+            } else {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])
+                        || ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[1])) {
+                    Toast.makeText(Roulette.this, "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요.",
+                            Toast.LENGTH_LONG).show();
+                    finish();
+                } else {
+                    Toast.makeText(Roulette.this, "퍼미션이 거부되었습니다. 설정에서 퍼미션을 허용해야 합니다.",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    void checkRunTimePermission() {
+        int hasFineLocationPermission = ContextCompat.checkSelfPermission(Roulette.this, Manifest.permission.ACCESS_FINE_LOCATION);
+        int hasCoarseLocationPermission = ContextCompat.checkSelfPermission(Roulette.this, Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED && hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED) {}
+        else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(Roulette.this, REQUIRED_PERMISSIONS[0])) {
+                Toast.makeText(Roulette.this, "이 앱을 실행하려면 위치 접근 권한이 필요합니다.", Toast.LENGTH_LONG).show();
+                ActivityCompat.requestPermissions(Roulette.this, REQUIRED_PERMISSIONS, PERMISSONS_REQUEST_CODE);
+            }
+            else {
+                ActivityCompat.requestPermissions(Roulette.this, REQUIRED_PERMISSIONS, PERMISSONS_REQUEST_CODE);
+            }
+        }
+    }
+
+    public String getCurrentAddress(double latitude, double longitude) {
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        List<Address> addresses;
+
+        try {
+            addresses = geocoder.getFromLocation(latitude, longitude, 7);
+        } catch (IOException ioException) {
+            Toast.makeText(this, "지오코더 서비스 사용 불가", Toast.LENGTH_LONG).show();
+            return "지오코더 서비스 사용 불가";
+        } catch (IllegalArgumentException illegalArgumentException) {
+            Toast.makeText(this, "잘못된 GPS 좌표", Toast.LENGTH_LONG).show();
+            return "잘못된 GPS 좌표";
+        }
+
+        if (addresses == null || addresses.size() == 0) {
+            Toast.makeText(this, "주소 미발견", Toast.LENGTH_LONG).show();
+            return "주소 미발견";
+        }
+
+        Address address = addresses.get(0);
+        return address.getAddressLine(0).toString()+"\n";
+    }
+
+    private void showDialogForLocationServiceSetting() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(Roulette.this);
+        builder.setTitle("위치 서비스 비활성화");
+        builder.setMessage("앱을 사용하기 위해서는 위치 서비스가 필요합니다.\n" + "위치 설정을 수정하실래요?");
+        builder.setCancelable(true);
+        builder.setPositiveButton("설정", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent callGPSSettingIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivityForResult(callGPSSettingIntent, GPS_ENABLE_REQUEST_CODE);
+            }
+        });
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.create().show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case GPS_ENABLE_REQUEST_CODE:
+                if (checkLocationServicesStatus()) {
+                    Log.d("@@@", "onActivityResult : GPS 활성화 되어있음");
+                    checkRunTimePermission();
+                    return;
+                }
+                break;
+        }
+    }
+
+    public boolean checkLocationServicesStatus() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
     }
 }
